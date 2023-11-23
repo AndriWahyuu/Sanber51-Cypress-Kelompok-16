6200e5529ca597537222bd9ff69b360345c22b5b
jaaah6 <izzahtazkiyah00@gmail.com>
2023-11-23 23:05:04 +0700

    proceedToCheckout 003

--- cypress/e2e/proceedToCheckout/proceedToCheckoutMagento.cy.js
+++ cypress/e2e/proceedToCheckout/proceedToCheckoutMagento.cy.js
@@ -1,5 +1,17 @@
-describe('template spec', () => {
+describe('Verify Procees to Checkout', () => {
+  beforeEach(() => {
+
+    cy.visit('https://magento.softwaretestingboard.com/')
+    cy.get('.authorization-link').eq(0).click() //untuk menghindari error can only be called on a single element
+    cy.get('#email').type('testizzah@gmail.com')
+    cy.get('#pass').type('Testizzah123')
+    cy.get('.action.login.primary').click()
+    cy.get('.action.showcart').click()
+    cy.get('.action.primary.checkout').click()
+  
+  })
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    
+
   })
 })
