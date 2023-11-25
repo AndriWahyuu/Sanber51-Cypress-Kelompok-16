60145281509189e22402a09a5b5845ae02ca87c1
jaaah6 <izzahtazkiyah00@gmail.com>
2023-11-25 14:46:47 +0700

    proceedToCheckout 008

--- cypress.config.js
+++ cypress.config.js
@@ -2,14 +2,14 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://magento.softwaretestingboard.com/',
+    baseUrl: 'https://magento.softwaretestingboard.com',
     env: {
       username: ''
     },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    defaultCommandTimeout: 5500,
+    defaultCommandTimeout: 10000,
     screenshotOnRunFailure: false,
     chromeWebSecurity: false
   },
--- cypress/e2e/proceedToCheckout/proceedToCheckoutMagento.cy.js
+++ cypress/e2e/proceedToCheckout/proceedToCheckoutMagento.cy.js
@@ -4,20 +4,22 @@ const userDataShipping  = require('../../fixtures/userDataShipping.json')
 describe('Verify Procees to Checkout', () => {
   beforeEach(() => {
 
-    cy.visit('https://magento.softwaretestingboard.com/')
-    cy.get('.authorization-link').eq(0).click() //untuk menghindari error can only be called on a single element
-    cy.get('#email').type('testingizzah@gmail.com')
-    cy.get('#pass').type('Testingizzah123')
-    cy.get('.action.login.primary').click()
-    cy.get('.action.showcart').click()
-    //cy.get('#top-cart-btn-checkout').click()
-    //cy.get('.action.primary.checkout').click({force: true})
+    cy.visit('');
+    cy.get('.authorization-link').eq(0).click(); //untuk menghindari error can only be called on a single element
+    cy.get('#email').type('testingizzah@gmail.com');
+    cy.get('#pass').type('Testingizzah123');
+    cy.get('.action.login.primary').click();
+    cy.get('.action.showcart').click();
+    //cy.get('#top-cart-btn-checkout').click();
+    //cy.get('action.primary.checkout').click({force: true});
     cy.get(".showcart").should("be.visible").click();
   })
   it('Terdapat produk yang akan dibeli di keranjang dan belum memasukkan data checkout', () => {
-    cy.get(".checkout-methods-items > :nth-child(1) > .action").should("be.visible").click();
-    cy.url().should('include', 'checkout/#shipping.html')
-    //cy.url().should('include', 'https://magento.softwaretestingboard.com/')
-    cy.get('#YO17RYX').type(userDataShipping.validUser.firstName)
+    cy.get('.action.primary.checkout').contains('Proceed to Checkout').should("be.visible").click();
+    cy.wait(5000)
+    cy.visit('https://magento.softwaretestingboard.com/checkout/#shipping');
+    cy.url().should('include', 'https://magento.softwaretestingboard.com/checkout/#shipping');
+    cy.get('[name="firstname"]').type(userDataShipping.validUser.firstName);
+    
   })
 })
