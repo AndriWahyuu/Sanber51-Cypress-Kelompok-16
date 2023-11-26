b4c3eba99c3edcd23116447d9afc067cee471a92
r204 <60972509+r204@users.noreply.github.com>
2023-11-26 09:24:08 +0700

    Create negative test
    login with a valid email & invalid password

--- cypress/e2e/3-web-lamomagento/login.cy.js
+++ cypress/e2e/3-web-lamomagento/login.cy.js
@@ -8,6 +8,15 @@ describe('Login Verivication Test', () => {
     cy.get('.action.login.primary').click()
     cy.get('.message-error').should('contain', 'The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.')
   })
+  it('Login failed 2', () => {
+    cy.visit('https://magento.softwaretestingboard.com/')
+    cy.contains('Sign In').click()
+    cy.get('.authorization-link').eq(0).click()
+    cy.get('#email').type('agung@gmail.com')
+    cy.get('#pass').type('test12345')
+    cy.get('.action.login.primary').click()
+    cy.get('.message-error').should('contain', 'The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.')
+  })
   it('Login Pass', () => {
     cy.visit('https://magento.softwaretestingboard.com/')
     cy.contains('Sign In').click()
