fdf94821ce9c8aff4e74878ee289edb75178d280
r204 <60972509+r204@users.noreply.github.com>
2023-11-26 08:36:17 +0700

    Create Negative Test Case
    Login with invalid data

--- cypress/e2e/3-web-lamomagento/login.cy.js
+++ cypress/e2e/3-web-lamomagento/login.cy.js
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Login Verivication Test', () => {
+  it('Login failed', () => {
     cy.visit('https://magento.softwaretestingboard.com/')
+    cy.contains('Sign In').click()
+    cy.get('.authorization-link').eq(0).click()
+    cy.get('#email').type('agung@test.com')
+    cy.get('#pass').type('test12345')
+    cy.get('.action.login.primary').click()
+    cy.get('.message-error').should('contain', 'The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.')
   })
 })
