98ccd46f0665780333cabb4e0e8eda725f37d533
r204 <60972509+r204@users.noreply.github.com>
2023-11-26 08:55:07 +0700

    Add Positive test case
    Login to the system with a valid account

--- cypress/e2e/3-web-lamomagento/login.cy.js
+++ cypress/e2e/3-web-lamomagento/login.cy.js
@@ -8,4 +8,13 @@ describe('Login Verivication Test', () => {
     cy.get('.action.login.primary').click()
     cy.get('.message-error').should('contain', 'The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.')
   })
+  it('Login Pass', () => {
+    cy.visit('https://magento.softwaretestingboard.com/')
+    cy.contains('Sign In').click()
+    cy.get('.authorization-link').eq(0).click()
+    cy.get('#email').type('agung@gmail.com')
+    cy.get('#pass').type('Agung12345')
+    cy.get('.action.login.primary').click()
+    //cy.get('.message-error').should('contain', 'The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.')
+  })
 })
